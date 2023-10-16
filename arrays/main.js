0bccfbfde06dd4a9c6e03e6db7e41203be9c4068
K1rioka <eshopokov@gmail.com>
2023-10-16 23:00:54 +0630

    pls accept other ones too

---
+++ arrays/main.js
@@ -0,0 +1,49 @@
+//// 1 Создание объекта
+// let person = {
+//     name: "John",
+//     age: 30,
+//     city: "New York"
+// };
+
+// alert(person);
+
+// // Изменение объекта
+// person.age = 20
+// alert(person)
+
+// // Длина массива
+// let numbers = [1, 1, 1, 1, 1, 1, 1]
+// alert(numbers.length)
+
+// // Добавление элементов в массив
+// let animals = []
+// animals.push = ('lion, tiger, cat, fugu_fish')
+// console.log(animals)
+
+// // Объект в массиве
+// let people = [
+//     { name: "Байсал", age: 99, city: "Мумбаи" },
+//     { name: "Арсен", age: 10, city: "Бразилиа" },
+//     { name: "Эмир", age: 20, city: "Найт Сити" },
+//     { name: "Алексей", age: 28, city: "Минск" }
+// ]
+// console.log(people)
+
+// // Массив с разными типами данных
+// let data = [16, "Cтрокf", { key: "Jбъект" }];
+
+// console.log(data);
+
+// // Доступ к элементам массива
+// let fruits = [pear, apple, pineapple, blueberry]
+
+// console.log(fruits[2])
+
+// // Доступ к свойствам объекта
+// let book = {
+//     title: 'Song of Ice and Fire',
+//     author: 'George Martin',
+//     year: 1996
+// }
+
+// console.log(book.author)
