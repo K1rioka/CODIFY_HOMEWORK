767a499b0d828fe2076932086a8278070f8d459c
K1rioka <eshopokov@gmail.com>
2023-10-16 22:40:41 +0630

    PLEASE ACCEPT IT TOO

---
+++ Data_Type/main.js
@@ -0,0 +1,22 @@
+// //Задача 1: Основы переменных
+// let name =prompt('Введите ваше имя:')
+// let age = prompt('Введите ваш возраст:')
+// let city = prompt('Введите ваш город:')
+
+// alert("Имя: " + name);
+// alert("Возраст: " + age);
+// alert("Город: " + city);
+
+// // Задача 2: Типы данных
+// let fav_num = parseInt(prompt(''));
+// let fav_color = prompt('');
+
+// console.log("Тип данных переменной любимое_число: " + typeof fav_num);
+// console.log("Тип данных переменной любимый_цвет: " + typeof fav_color);
+
+// // Задача 3: Строки
+// let first_word = prompt('');
+// let second_word = prompt('');
+// let merge = first_word + " " + second_word;
+
+// console.log(merge);
